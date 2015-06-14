f6eac4dc784ba73c95a09e1cf45d665f2c4bc19a
Andy3942 <bzx@bzx-mactekiiMac.local>
2015-06-14 22:15:00 +0800

    “”

---
+++ ClassPath.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import os
+
+root_path = "/Users/bzx/Documents/git/mygame/src/"
+path_star_index = len(root_path)
+
+dirs = [
+	"/Users/bzx/Documents/git/mygame/src/my/",
+]
+
+to_file_path = "/Users/bzx/Documents/git/mygame/src/my/load/ClassPath.lua"
+
+fileter_dirs = [".svn"]
+class ClassPath:
+	_data = ""
+	def findFilePath(self, path):
+		if os.path.isdir(path):
+			for sub_dir in os.listdir(path):
+				if self.isFiltered(sub_dir):
+					continue
+				sub_path = os.path.join(path, sub_dir)
+				self.findFilePath(sub_path)
+		elif path.endswith(".lua"):
+			self._data = self._data + "\n[\"%s\"] = \"%s\""%(os.path.basename(path)[:-4], path[path_star_index:-4])
+		pass
+
+	def initData(self):
+		self._data = "ClassPath = {"
+		for dir_temp in dirs:
+			self.findFilePath(dir_temp)
+			pass
+		self._data = self._data + "\n}"
+		pass
+
+	def isFiltered(self, dir_name):
+		for fileter_dir in fileter_dirs:
+			if fileter_dir == dir_name:
+				return True
+		return False
+		pass
+
+	def save(self):
+		with open(to_file_path, "w") as fp:
+			fp.write(self._data)
+			fp.close()
+		pass
+class_path = ClassPath()
+class_path.initData()
+class_path.save()
